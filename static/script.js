8fec3645e07efb8eb44e2d344d9ca53a659bb5d3
Simon <simon@siku2.io>
2022-10-01 14:14:24 +0000

    prettify

--- static/script.js
+++ static/script.js
@@ -1,23 +1,26 @@
 (async () => {
-    const response = await fetch('https://api.github.com/repos/dsg-jam/LD51/contents/?ref=github-pages');
-    const data = await response.json();
+  const response = await fetch(
+    "https://api.github.com/repos/dsg-jam/LD51/contents/?ref=github-pages"
+  );
+  const data = await response.json();
 
-    let htmlString = '';
-    let paths = [];
+  let htmlString = "";
+  let paths = [];
 
-    data.sort((a, b) => b.name.localeCompare(a.name))
+  data.sort((a, b) => b.name.localeCompare(a.name));
 
-    for (let file of data) {
-        if (file.type === "dir" && /^\d/.test(file.name)) {
-            paths.push(file.path)
-            htmlString += `<span><a href="${file.path}">${file.name}</a></span><br/>`;
-        }
+  for (let file of data) {
+    if (file.type === "dir" && /^\d/.test(file.name)) {
+      paths.push(file.path);
+      htmlString += `<span><a href="${file.path}">${file.name}</a></span><br/>`;
     }
+  }
 
-    if (paths.length > 0) {
-        htmlString = `<span><a href="${paths.sort().reverse()[0]}">Latest</a></span><br/>` + htmlString;
-    }
+  if (paths.length > 0) {
+    htmlString =
+      `<span><a href="${paths.sort().reverse()[0]}">Latest</a></span><br/>` +
+      htmlString;
+  }
 
-
-    document.getElementsByClassName('dsg-links')[0].innerHTML = htmlString;
-})()
+  document.getElementsByClassName("dsg-links")[0].innerHTML = htmlString;
+})();
