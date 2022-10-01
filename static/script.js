3e5e06ea86de43fc07d60b497776777e6ed94cf5
mineiwik <29404520+mineiwik@users.noreply.github.com>
2022-10-01 11:57:21 +0200

    update style

--- static/script.js
+++ static/script.js
@@ -1,14 +1,12 @@
 (async () => {
     const response = await fetch('https://api.github.com/repos/mineiwik/LD51/contents/?ref=github-pages');
     const data = await response.json();
-    let htmlString = '<ul>';
+    let htmlString = '';
 
     for (let file of data) {
         if (file.type === "dir" && /^\d/.test(file.name)) {
-            htmlString += `<li><a href="${file.path}">${file.name}</a></li>`;
+            htmlString += `<span><a href="${file.path}">${file.name}</a></span><br/>`;
         }
     }
-
-    htmlString += '</ul>';
     document.getElementsByClassName('dsg-links')[0].innerHTML = htmlString;
 })()
