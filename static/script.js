9d3fad2aea0a940799ab458172cffedeb5edbc41
mineiwik <29404520+mineiwik@users.noreply.github.com>
2022-10-01 12:07:06 +0200

    add latest link

--- static/script.js
+++ static/script.js
@@ -1,12 +1,21 @@
 (async () => {
     const response = await fetch('https://api.github.com/repos/mineiwik/LD51/contents/?ref=github-pages');
     const data = await response.json();
+
     let htmlString = '';
+    let paths = [];
 
     for (let file of data) {
         if (file.type === "dir" && /^\d/.test(file.name)) {
+            paths.push(file.path)
             htmlString += `<span><a href="${file.path}">${file.name}</a></span><br/>`;
         }
     }
+
+    if (paths.length > 0) {
+        htmlString = `<span><a href="${paths.sort().reverse()[0]}">Latest</a></span><br/>` + htmlString;
+    }
+
+
     document.getElementsByClassName('dsg-links')[0].innerHTML = htmlString;
 })()
