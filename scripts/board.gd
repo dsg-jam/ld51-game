8393ea4cc872393d259ebf1d65f17c314bc0bab6
mineiwik <29404520+mineiwik@users.noreply.github.com>
2022-10-02 13:06:05 +0200

    refactoring

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,14 +1,10 @@
 extends Node2D
 
-
-const MAX_BOARD_DIMENSIONS = Vector2(1,1)
-
 @onready var texture = $Texture
 @onready var tile_prefab = preload("res://prefabs/tile.tscn")
 
 func _ready():
-	render_board("oo;;o;xxxxxx;xxxoxx;ooooo;oooxxxx;xx;ooox;xxo;oxxxxx;;;o;")
-
+	render_board("oo;;o;xxxxxx;xxxoxx;ooooo;;oooxxxx;xx;ooox;xxo;oxxxxx;;;o;")
 
 func render_board(input: String):
 	"""
@@ -17,30 +13,17 @@ func render_board(input: String):
 			'o': empty tile (void - player can fall down) - all board surrounding tiles are implicitly empty tiles
 			';': line break
 	"""
-	
-	var regex_line_end = RegEx.new()
-	regex_line_end.compile("(o*;)")
-	
-	var regex_top_down = RegEx.new()
-	regex_top_down.compile("(^;*)|(;*$)")
-	
-	input = regex_line_end.sub(input, ";", true)
-	input = regex_top_down.sub(input, "", true)
-	
-	var board_grid = input.split(";")
-	
-	var max_width = 0
-	for line in board_grid:
-		if len(line) > max_width:
-			max_width = len(line)
+	var board_grid = get_board_grid(input)	
+	var tile_size = get_tile_size(board_grid)
+	draw_board(board_grid, tile_size)
 
-	var tile_size = min(floor(texture.polygon[2].x/max_width), floor(texture.polygon[2].y)/board_grid.size())
-	
-	print(tile_size)
-	
-	for i in board_grid.size():
-		for j in len(board_grid[i]):
-			match board_grid[i][j]:
+func _process(delta):
+	pass
+
+func draw_board(board_grid: Array, tile_size: int):
+	for row in board_grid.size():
+		for col in board_grid[row].size():
+			match board_grid[row][col]:
 				"x":
 					var new_tile = tile_prefab.instantiate()
 					new_tile.get_node("Texture").set_polygon(
@@ -52,10 +35,37 @@ func render_board(input: String):
 						]
 					)
 					add_child(new_tile)
-					new_tile.position = Vector2(j, i) * tile_size
-					
+					new_tile.position = Vector2(col, row) * tile_size
 				"o":
 					pass
 
-func _process(delta):
-	pass
+func get_board_grid(input: String) -> Array:
+	input = clean_up_input(input)
+	var board_grid = []
+	for line in input.split(";"):
+		var grid_line = []
+		for character in line:
+			grid_line.append(character)
+		board_grid.append(grid_line)
+	return board_grid
+	
+
+func clean_up_input(input: String) -> String:
+	var regex_line_end = RegEx.new()
+	regex_line_end.compile("(o*;)")
+	
+	var regex_top_down = RegEx.new()
+	regex_top_down.compile("(^;*)|(;*$)")
+	
+	input = regex_line_end.sub(input, ";", true)
+	input = regex_top_down.sub(input, "", true)
+	
+	return input
+
+func get_tile_size(board_grid: Array) -> int:
+	var max_width = 0
+	for line in board_grid:
+		if line.size() > max_width:
+			max_width = line.size()
+
+	return min(floor(texture.polygon[2].x/max_width), floor(texture.polygon[2].y)/board_grid.size())
