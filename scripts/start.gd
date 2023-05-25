bcf2fcbd3863e09ee87b9ea1c5b72bf22aa13172
mineiwik <29404520+mineiwik@users.noreply.github.com>
2023-05-26 01:10:42 +0300

    joins lobby on enter key pressed (#50)

--- scripts/start.gd
+++ scripts/start.gd
@@ -4,16 +4,24 @@ extends Control
 @export var _lobby_networking: LobbyNetworking
 
 
-func _on_create_lobby_button_pressed():
-	self._lobby_networking.create_lobby()
-
-
-func _on_join_button_pressed():
+func _join_lobby():
 	var join_code = _join_code_input.text
 	if len(join_code) <= 0:
 		return
 	self._lobby_networking.join_lobby(join_code)
 
 
+func _on_create_lobby_button_pressed():
+	self._lobby_networking.create_lobby()
+
+
+func _on_join_button_pressed():
+	self._join_lobby()
+
+
 func _on_paste_button_pressed():
 	self._join_code_input.text = DisplayServer.clipboard_get()
+
+
+func _on_join_code_text_submitted(_new_text):
+	self._join_lobby()
